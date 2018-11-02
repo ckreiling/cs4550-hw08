3367d32a5e1b7b7e0b1586971f6166189c9338ed
Christian Kreiling <christian.kreiling@gmail.com>
2018-11-02 12:44:38 -0400

    Setup redux store provider

--- assets/js/app/index.js
+++ assets/js/app/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Provider } from "react-redux";
+
+import store from "./store";
 
 /**
  * The root of the SPA.
  * @param {React.Props} props
  */
 export default function(props) {
-  return <div className="appContainer">Hello, world!</div>;
+  return (
+    <Provider store={store}>
+      <div className="appContainer">Hello, world!</div>
+    </Provider>
+  );
 }
