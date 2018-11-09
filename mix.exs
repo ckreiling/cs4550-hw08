e023c8d039ab7afc90fbabb8d3b1e548a772850c
Christian Kreiling <christian.kreiling@gmail.com>
2018-11-09 14:48:21 -0500

    added distillery

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Todos.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:comeonin, "~> 4.1"},
-      {:argon2_elixir, "~> 1.3"}
+      {:argon2_elixir, "~> 1.3"},
+      {:distillery, "~> 2.0"}
     ]
   end
 
